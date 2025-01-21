1eba1c2924d1b3ca44be844a44323db918c82212
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2025-01-21 13:58:24 +0100

    Update VACUUM

--- adminEndpoints.js
+++ adminEndpoints.js
@@ -1,12 +1,16 @@
 const db = require('./database');
 
+// GET <ip:port>/api/vacuum
 exports.vacuum = (req, res) => {
     db.run('VACUUM', (err) => {
         if (err) {
             console.error('Error running VACUUM:', err.message);
-        } else {
-            console.log('VACUUM completed successfully');
+            res.status(500).json({ error: err.message });
+            return;
         }
+    
+        console.log('VACUUM completed successfully');
+        res.status(200).send('VACUUM completed');
     });
 };
 
