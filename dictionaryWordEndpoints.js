92e0f97bfc605fab4137520e3f9701d6e08cc03a
Ian Brown <18260644+ianabrown@users.noreply.github.com>
2025-01-29 13:44:22 +0100

    Refactoring. Complete at this point

--- dictionaryWordEndpoints.js
+++ dictionaryWordEndpoints.js
@@ -1,7 +1,4 @@
 // External requires
-const fs = require('fs').promises;
-const os = require('os');
-const path = require('path');
 
 // Internal requires
 const db = require('./database');
@@ -9,84 +6,39 @@ const db = require('./database');
 // The main table
 const tableName = 'dictionaryWordList';
 
+// Endpoint to confirm whether :word exists in the word list table
+// Returns a JSON structure
 exports.match = (req, res) => {
     const word = req.params.word;
 
     console.debug(`Calling /api/dictionary/match/${word}`);
 
-    try {
-        const exists = matchWord(word, (err, result) => {
-            if (err) {
-                res.status(500).json({ error: err.message });
-            }
+    matchWord(word, (err, result) => {
+        if (err) {
+            res.status(500).json({ error: err.message });
+        }
 
-            return res.json({ word: word, exists: result });
-        });
-    } catch (error) {
-        console.error('Error executing query:', error);
-
-        res.status(500).json({ error: error.message });
-    }
+        return res.json({ word: word, exists: result });
+    });
 }
 
-// Returns whether there are words in the dictionary that start with `:letters`
-// Return is a JSON structure
-// TODO See if performance would benefit from a simpler return value
-// TODO See if this is just impractical from a performance point of view
-// TODO RESTRUCTURE THIS TO UPLOAD THE WHOLE GRID - DO NOT MAKE INDIVIDUAL WEB CALLS TO THIS
-// TODO TURN THIS INTO A REGULAR API CALL FROM A 'SOLVE PUZZLE' ENDPOINT 
-// TODO Can we do a shutdown via API? And if we run with nodemon, would that be a restart?
+// Endpoint to confirm whether there are words in the word list table that _start with_ `:letters`
+// Returns a JSON structure
 exports.partialMatch = (req, res) => {
     const letters = req.params.letters;
 
     console.debug(`Calling /api/dictionary/partial/${letters}`);
 
-    const query = `SELECT EXISTS(SELECT 1 FROM ${tableName} WHERE word LIKE ?) AS exists_flag`;
-
-    db.get(query, [`${letters}%`], (err, row) => {
+    matchPartialWord(letters, (err, result) => {
         if (err) {
-            console.error('Error executing query:', err);
             res.status(500).json({ error: err.message });
-            return;
         }
 
-        const exists = row.exists_flag === 1;
-        res.json({ letters: letters, exists: exists });
+        return res.json({ letters: letters, exists: result });
     });
 };
 
-// Inserts the contents of the `words` array into the dictionary table
-// TODO consider using express file upload
-// TODO delete current database contents
-// TODO make sure to use an insert method reasonable for performance 
-// TODO return word/insert count
-exports.installWords = (req, res) => {
-    const { words } = req.body;
-    if (!words || !Array.isArray(words)) {
-        res.status(400).json({ error: 'Array of words is required' });
-        return;
-    }
-
-    const stmt = db.prepare(`INSERT OR IGNORE INTO ${tableName} (word) VALUES (?)`);
-    let addedCount = 0;
-
-    // db.serialize(() => {
-
-    // });
-
-    words.forEach((word) => {
-        db.run(`INSERT OR IGNORE INTO ${tableName} (word) VALUES (?)`, [word], function (err) {
-            if (err) {
-                res.status(500).json({ error: err.message });
-                return;
-            }
-            //            res.json({ id: this.lastID, name, price });
-        });
-    });
-
-    res.status(200).json({ status: "Complete" });
-};
-
+// Endpoint that returns the number of entries in the word list table
 exports.info = (req, res) => {
     console.debug(`Get dictionary info`);
 
@@ -103,33 +55,35 @@ exports.info = (req, res) => {
     });
 }
 
+// Endpoint to upload a text file and have its contents injected into the word list table, deleting the existing content
+// Returns a JSON structure
 exports.upload = (req, res) => {
     if (!req.files || Object.keys(req.files).length === 0) {
         return res.status(400).send('No files were uploaded.');
     }
 
-    // * use uploadedFile.data instead of this mv
-    // * tidy this implementation or get deepseek to write a sync import for one of our other implementationss
-
-    // Upload the file to a temp folder and then import it
+    // Get the file from the input. 
+    // Its contents will be in 'uploadedFile.data' and we will need to call 'toString' on it before use
     let uploadedFile = req.files.file;
 
     console.debug(`Importing words from ${uploadedFile.name}`);
 
     // Get the data and split it into an array of 'words' - an unsanitized list at this point
+    // TODO consider excluding words of less than 4 chars
+    // TODO finally decide whether we like this a-z test
     const data = uploadedFile.data;
-    const words = data.toString('utf8').split(/\s+/).filter(word => word.length > 0);
+    const words = data.toString('utf8').split(/\s+/).filter(word => word.length > 0 && /^[a-zA-Z]*$/.test(word));
 
     if (words === undefined || words.length == 0) {
         throw new Error('File does not contain a word list');
     }
 
-    importWordList(words, (err) => {
+    importWordList(words, (err, count) => {
         if (err) {
             res.status(500).json({ message: 'An error occurred', error: err.message });
         }
 
-        res.status(200).json({ status: "Complete" });
+        res.status(200).json({ status: "Complete", words: words.length, imported: count });
     });
 }
 
@@ -146,7 +100,22 @@ async function matchWord(word, callback) {
     });
 }
 
+// Returns a [err, boolean] on whether any words starting with 'letters' are in the dictionary
+async function matchPartialWord(letters, callback) {
+    console.debug(`Searching for ${letters}.* in the dictionary`);
+
+    // Append '%' to the provided value to do a SQL wildcard search for words starting with 'letters'
+    db.get(`SELECT EXISTS (SELECT 1 FROM ${tableName} WHERE word LIKE ?) AS exists_flag`, [`${letters}%`], (err, rows) => {
+        if (err) {
+            return callback(err, null);
+        }
+
+        callback(null, rows.exists_flag === 1);
+    });
+}
+
 // Import the word list, replacing the current contents, as a transaction
+// Returns [err, wordcount]. Word count will differ from words.length if any of the individual inserts failed. Unlikely. 
 async function importWordList(words, callback) {
     db.serialize(() => {
         db.run("BEGIN TRANSACTION");
@@ -155,10 +124,13 @@ async function importWordList(words, callback) {
 
         const stmt = db.prepare(`INSERT OR IGNORE INTO ${tableName} (word) VALUES (?)`);
 
+        let counter = 0;
         for (const word of words) {
             stmt.run(word.toLowerCase().trim(), (err) => {
                 if (err) {
                     console.error('Error inserting row:', err.message);
+                } else {
+                    counter++;
                 }
             });
         }
@@ -168,10 +140,10 @@ async function importWordList(words, callback) {
         db.run("COMMIT", (err) => {
             if (err) {
                 console.error('Error committing transaction:', err.message);
-                callback(err);
+                callback(err, null);
             } else {
                 console.log('All inserts completed successfully');
-                callback(null);
+                callback(null, counter);
             }
         });
     });
